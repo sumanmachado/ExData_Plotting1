7374183fa22f31e4fc3de5db0c257fb255b0887d
Suman Machado <sumanmachado@gmail.com>
2015-06-05 16:36:51 +0200

    adding plot1

---
+++ Plot1.R
@@ -0,0 +1,10 @@
+setwd("R")
+house_data <- read.table ("household_power_consumption.txt", fill=FALSE, strip.white=TRUE, na.string='?', sep=';', header=TRUE)
+
+## Reformatting the date
+house_data$Date <- as.Date( as.character(house_data$Date), "%d/%m/%Y")
+house_dateSelect <- subset(house_data, Date >= as.Date("2007-02-01") & Date < as.Date("2007-02-03"))
+
+hist(house_dateSelect$Global_active_power, col="red", main="Gobal Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
+dev.copy(png,'plot1.png',width = 480, height = 480, units = "px")
+dev.off()
